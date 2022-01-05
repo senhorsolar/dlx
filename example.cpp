a667ff7231ee61f9f9fcca87f3b5d71b2f023b9e
senhorsolar <25336720+senhorsolar@users.noreply.github.com>
2022-01-04 19:56:37 -0500

    testing rand matrix

--- example.cpp
+++ example.cpp
@@ -10,16 +10,16 @@ template<class T>
 Matrix<T> gen_rand_matrix(size_t n)
 {
     Matrix<T> mat;
-    for (size_t i = 0; i < n / 2; ++i) {
+    for (size_t i = 0; i < n; ++i) {
 	std::vector<T> row1;
-	std::vector<T> row2;
+	//	std::vector<T> row2;
 	for (size_t j = 0; j < n; ++j) {
 	    float r = (float)rand() / (float)RAND_MAX;
 	    row1.push_back((T)((r > 0.5) ? 1 : 0));
-	    row2.push_back((T)((row1[j] == 0) ? 1 : 0));
+	    //  row2.push_back((T)((row1[j] == 0) ? 1 : 0));
 	}
 	mat.push_back(std::move(row1));
-	mat.push_back(std::move(row2));
+	//	mat.push_back(std::move(row2));
     }
     return mat;
 }
@@ -35,7 +35,8 @@ std::vector<std::string> gen_column_names(size_t n)
 int main()
 {
     using namespace std;
-    
+
+    srand(0);
     vector<string> column_names;
     column_names = {"A", "B", "C", "D", "E", "F", "G"};
     vector<vector<bool> > test_matrix{{0, 0, 1, 0, 1, 1, 0},
@@ -45,7 +46,7 @@ int main()
 				      {0, 1, 0, 0, 0, 0, 1},
 				      {0, 0, 0, 1, 1, 0, 1}};
 
-    size_t n = 50;
+    size_t n = 1000;
     test_matrix = gen_rand_matrix<bool>(n);
     column_names = gen_column_names(n);
     
@@ -55,13 +56,13 @@ int main()
     std::cout << dlx.SolutionCount() << '\n';
 
     for (const auto& solution : dlx.GetSolutions()) {
-	std::cout << "solution:\n";
+	//	std::cout << "solution:\n";
 	auto decoded_solution = dlx.DecodeSolution(column_names, solution);
 	for (auto& row : decoded_solution) {
 	    for (auto& s : row) {
-		std::cout << s << " ";
+		//	std::cout << s << " ";
 	    }
-	    std::cout << '\n';
+	    //std::cout << '\n';
 	}
     }
 
